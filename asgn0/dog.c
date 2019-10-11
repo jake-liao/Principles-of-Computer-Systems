5e0ae0fabf3430c626aeef30184eea45d93688e4
Jake <hsliao@ucsc.edu>
2019-10-10 17:12:03 -0700

    fix comments

--- asgn0/dog.c
+++ asgn0/dog.c
@@ -28,34 +28,31 @@ char *getPath(char *filename)
 
 void copyFile(uint8_t fd)
 {
-	// read file
+    // read file
     uint16_t fileSize = (uint16_t)lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);
-    int EOFFlag = getchar();
-    if(EOFFlag == EOF) {
-        exit(EXIT_SUCCESS);
-    } else {
-        uint16_t residualBytes = fileSize;
-        do {
-            if(residualBytes > BUFMAX) {
-                /* rest of file is larger than BUFMAX
-                use BUFMAX as buffer size */
-                char *buf = (char *)malloc(BUFMAX);
-                read(fd, buf, BUFMAX);
-                write(STDOUT_FILENO, buf, BUFMAX);
-                free(buf);
-            } else {
-                /* remaining file is smaller than BUFMAX
-                use remaining byte size as buffer */
-                char *buf = (char *)malloc(residualBytes);
-                read(fd, buf, residualBytes);
-                write(STDOUT_FILENO, buf, residualBytes);
-                free(buf);
-            }
-            residualBytes = fileSize - (uint16_t)lseek(fd, 0, SEEK_CUR);
-            // get rest of file
-        } while(residualBytes > 0);
-    }
+    uint16_t residualBytes = fileSize;
+    do {
+        if(residualBytes > BUFMAX) {
+            /* rest of file is larger than BUFMAX
+            use BUFMAX as buffer size */
+            char *buf = (char *)malloc(BUFMAX);
+            read(fd, buf, BUFMAX);
+            write(STDOUT_FILENO, buf, BUFMAX);
+            memset(buf, 0, BUFMAX);
+            free(buf);
+        } else {
+            /* remaining file is smaller than BUFMAX
+            use remaining byte size as buffer */
+            char *buf = (char *)malloc(residualBytes);
+            read(fd, buf, residualBytes);
+            write(STDOUT_FILENO, buf, residualBytes);
+            memset(buf, 0, residualBytes);
+            free(buf);
+        }
+        residualBytes = fileSize - (uint16_t)lseek(fd, 0, SEEK_CUR);
+        // get rest of file
+    } while(residualBytes > 0);
     return;
 }
 
@@ -64,15 +61,18 @@ int main(int argc, char *argv[])
     if(argc == 1) {
         // no file, take input from stdin
         copyFile(STDIN_FILENO);
-        return (0);
+
+        exit(EXIT_SUCCESS);
 
     } else {
         for(uint8_t i = 1; i < argc; i++) {
             if(strcmp("-", argv[i]) == 0) {
                 // take input from stdin
-                copyFile(STDIN_FILENO);
+                while(getchar() != EOF) {
+                    copyFile(STDIN_FILENO);
+                }
             } else {
-            	// copy from file
+                // copy from file
                 char *path = getPath(argv[i]);
                 uint8_t fd = open(path, O_RDONLY);
                 copyFile(fd);
