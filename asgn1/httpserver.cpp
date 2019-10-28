6ebdf04f8406a7e9cfecc7af45ef8ecc2d0ae2dd
Jake <hsliao@ucsc.edu>
2019-10-28 14:34:35 -0700

    200, 201, 400, 500 added, DESIGN v3

--- asgn1/httpserver.cpp
+++ asgn1/httpserver.cpp
@@ -1,72 +1,102 @@
-#include<arpa/inet.h> 
-#include<cstdlib>
-#include<netdb.h> 
-#include<netinet/in.h> 
-#include<stdio.h> 
-#include<string.h> 
-#include<sys/socket.h> 
-#include<sys/types.h> 
-#include<sys/un.h> 
-#include<unistd.h>
-
-#define BUFMAX 32768
-#define HOSTNAMESIZE 100
+/*------------------------------------------------------------------------------/
+CSE130 	Prof. Miller 		asgn1		httpserver.cpp
+hsliao	Jake Hsueh-Yu Liao	1551558
+/------------------------------------------------------------------------------*/
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <err.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
 #define ERR -1
-#define DEFAULTPORT 80
-int main(int argc, char *argv[])
-{
-	if (argc < 2){
-		printf("Missing arguments\n");
-		exit(EXIT_FAILURE);
-	}
-	int16_t s_fd;
-	char hostname[HOSTNAMESIZE];
-	struct sockaddr_in addr;
-	int addrlen = sizeof(addr); 
-	char buffer[1024] = {0};
+#define BUFMAX 32768
+#define STATUSMAX 11
+#define HEADERMAX 4096
 
-	s_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (s_fd == ERR){
-		printf("socket failure\n");
-		exit(EXIT_FAILURE);
-	}
+void handle_client(int8_t soc_fd) {
+  uint8_t buffer[BUFMAX] = {0};
+  uint8_t header[HEADERMAX] = {0};
+  // char response[HEADERMAX] ={0};
+  char command[4] = {0};
+  char filename[27] = {0};
+  char data[100] = {0};
+  char status[11] = {0};
+  int size = 0;
+  read(soc_fd, (char *)buffer, sizeof(buffer));
+  sscanf((char *)buffer, "%s %s %*s %*s %*s %*s %*s %*s %*s %d %s", command,
+         filename, &size, data);
+  strcpy((char *)header, "HTTP/1.1 ");
+  if (strcmp(command, "PUT") == 0) {
+    int8_t fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR);
+    if (fd == ERR) {
+      strcat((char *)header, "400 Bad Request\r\n");
+    } else {
+	  write(fd, data, sizeof(data));
+      strcat((char *)header, "201 Created\r\n");
+    }
 
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(DEFAULTPORT);
-	if (gethostname(hostname, sizeof(hostname)) == ERR){
-		printf("gethostname failure\n");
-		exit(EXIT_FAILURE);
-	}
+  } else if (strcmp(command, "GET") == 0) {
+    int8_t fd = open(filename, O_RDONLY);
+    if (fd == ERR) {
+      strcat((char *)header, "400 Bad Request\r\n");
+    } else {
+      strcat((char *)header, "200 OK\r\n");
+      read(fd, data, sizeof(data));
+      close(fd);
+      sprintf((char *)buffer, "Conten-Length: %d\r\n%s\r\n", sizeof(data), data);
+      strcat((char *)header, (char *)buffer);
+    }
+  } else {
+    strcat((char *)header, "500 Internal Server Error\r\n");
+  }
+  // printf("response->%s\n", header);
+  // strcpy(response, (char*)header);
+  if (send(soc_fd, (char*)header, HEADERMAX, 0) == ERR )
+  	err(1, "send() failed");
+  return;
+}
 
-	if (strcmp(argv[1], hostname) == 0 || strcmp(argv[1], "localhost") == 0){
-		printf("hostname or localhost detected\n");
-		addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	} else {
-		printf("assuming correct IPv4 address\n");
-		struct in_addr inAddr;
-		addr.sin_addr.s_addr = inet_aton(argv[1], &inAddr);
-	}
-	if (argc == 3){
-		addr.sin_port = htons(atoi(argv[2]));
-	}
-	if (bind(s_fd, (struct sockaddr*)&addr,sizeof(addr)) == ERR){
-		printf("bind failure\n");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(s_fd, 3) == ERR){
-		printf("listen failure\n");
-		exit(EXIT_FAILURE);
-	}
-	int16_t acc_soc = accept(s_fd, (struct sockaddr*)&addr, (socklen_t*)&addrlen);
-	if (acc_soc == ERR){
-		printf("accept failure\n");
-		exit(EXIT_FAILURE);
-	}
-	read(acc_soc, buffer, 1024);
-	printf("%s\n", buffer);
-	send(acc_soc, "fuck you", strlen("fuck you"), 0);
-	printf("message sent\n");
-	return 0;
-
+int main(int argc, char *argv[]) {
+  if (argc < 2)
+    err(1, "missing argument(s): argc: %d", argc);
+  struct addrinfo *addrs, hints = {};
+  int enable = 1;
+  int8_t N = 16;
+  int8_t main_socket;
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  if (argc == 3) {
+    getaddrinfo(argv[1], argv[2], &hints, &addrs);
+  } else {
+    getaddrinfo(argv[1], "80", &hints, &addrs);
+  }
+  if ((main_socket = socket(addrs->ai_family, addrs->ai_socktype,
+                            addrs->ai_protocol)) == ERR) {
+    err(1, "socket failure");
+  }
+  if (setsockopt(main_socket, SOL_SOCKET, SO_REUSEADDR, &enable,
+                 sizeof(enable)) == ERR) {
+    err(1, "setsockopt failure");
+  }
+  if (bind(main_socket, addrs->ai_addr, addrs->ai_addrlen) == ERR) {
+    err(1, "bind failure");
+  }
+  if (listen(main_socket, N) == ERR) {
+    err(1, "listen failure");
+  }
+  while (1) {
+    int16_t acc_soc;
+    if ((acc_soc = accept(main_socket, NULL, NULL)) == ERR) {
+      err(1, "accept failure");
+    }
+    handle_client(acc_soc);
+  }
+  return 0;
 }
